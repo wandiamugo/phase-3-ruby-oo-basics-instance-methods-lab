35c5eb527012bb724b943f4332541f5b757a3808
wandiamugo <wwandia173@gmail.com>
2022-08-17 15:22:39 +0300

    commit tree 437eee3f7b5268c52a3af7ba242d796be6aca881

---
+++ lib/dog.rb
@@ -0,0 +1,42 @@
+class Dog
+    def bark
+        puts "Woof!"
+    end
+end
+
+
+fido = Dog.new
+fido.bark
+
+snoopy = Dog.new
+snoopy.bark
+
+
+class Dog
+    def sit
+        puts "The Dog is sitting"
+    end
+end
+
+leylyz= Dog.new
+leylyz.sit
+
+class Person
+    def talk
+        puts "Hello World!"
+    end
+end
+
+mark = Person.new
+mark.talk
+
+class Person
+    def walk
+        puts "The Person is walking"
+    end
+end
+
+wandia = Person.new
+wandia.walk
+
+mark = Person.new
